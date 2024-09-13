b32b266a3f5f534c58d66b1a25731bddb677fede
Cristian <jaldinpereiracristian@gmail.com>
2024-09-13 17:16:34 -0400

    hoy

--- src/routes.py
+++ src/routes.py
@@ -31,19 +31,3 @@ def add_user():
 category_schema = CategorySchema()
 categories_schema = CategorySchema(many=True)
 
-# Ruta para ver todas las categorías
-@app.route('/categories', methods=['GET'])
-def get_categories():
-    categories = Category.query.all()
-    return categories_schema.jsonify(categories)
-
-# Ruta para crear una nueva categoría
-@app.route('/categories', methods=['POST'])
-def add_category():
-    name = request.form.get('name')
-    description = request.form.get('description')
-
-    new_category = Category(name=name, description=description)
-    db.session.add(new_category)
-    db.session.commit()
-    return redirect(url_for('home'))
